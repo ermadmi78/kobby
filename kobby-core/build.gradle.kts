e391209c84d3bc659f25e6f406ef6a841c0acdcd
Dmitry Ermakov <ermadmi78@gmail.com>
2020-10-17 20:50:02 +0300

    build project

---
+++ kobby-core/build.gradle.kts
@@ -0,0 +1,17 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+}
+group = "io.kobby"
+version = "0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+}
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+
+rootProject.name = "kobby"
+
+
+include(":kobby-core")
+include(":kobby-gradle-plugin")
+include(":kobby-maven-plugin")
+
